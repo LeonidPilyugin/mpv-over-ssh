d84d9fdb78c95356846073528f7611856f173ad3
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-04-07 17:37:04 +0300

    test

--- ms.sh
+++ ms.sh
@@ -5,37 +5,47 @@ source ~/.local/share/mpv-over-ssh/settings.sh
 handler () {
     while true; do
         if read line; then
-            echo got line $line
-            if [ line == "quit" ]; then
-                pkill mpv
-            else
-                scp "$1:$line" $3
-                bash "$4 $3" &
-            fi
+            echo got line $line >> /tmp/log.log
+            # if [ line == "quit" ]; then
+            #     pkill mpv
+            # else
+            #     scp "$1:$line" $3
+            #     bash "$4 $3" &
+            # fi
         fi
+        sleep 0.5
     done
 }
 
 prepare () {
-    mkdir -p $(dirname $3)
-    ssh $1 "rm $2; mkfifo $2"
+    if [ ! -f $(dirname $3) ]; then
+        mkdir -p $(dirname $3)
+    fi
+    ssh $1 "[ ! -e $2 ] && mkfifo $2"
+    if [[ ! -e $2 ]]; then
+        mkfifo $2
+    fi
 }
 
 finish () {
     ssh $1 "rm $2"
     rm -rf $(dirname $3)
-    kill $5
+    rm $2
 }
-
-run () {
-    ssh $1 "tail -f $2" | handler $@
-}
-
 
 HOST_NAME=$1
 
 prepare $HOST_NAME $FIFO_PATH $LOCAL_FILE
-run $HOST_NAME $FIFO_PATH $LOCAL_FILE $COMMAND &
-RUN_PID=$!
-ssh $@
-finish $HOST_NAME $FIFO_PATH $LOCAL_FILE $COMMAND $RUN_PID
+echo "jobs: $(jobs -l)"
+
+ssh $HOST_NAME "tail -f $FIFO_PATH" > $FIFO_PATH &
+SSH_PID=$!
+
+handler $HOST_NAME $FIFO_PATH $LOCAL_FILE $COMMAND < $FIFO_PATH &
+HANDLER_PID=$!
+
+echo "jobs: $(jobs -l)"
+read
+#ssh $@
+kill $SSH_PID $HANDLER_PID
+finish $HOST_NAME $FIFO_PATH $LOCAL_FILE $COMMAND
--- settings.sh
+++ settings.sh
@@ -1,4 +1,4 @@
 COMMAND="mpv --vo=drm --keep-open=yes"
 LOCAL_FILE="/tmp/mpv-over-ssh/current"
-FIFO_PATH="~/.local/share/mpv-over-ssh/queue"
+FIFO_PATH="$HOME/.local/share/mpv-over-ssh/queue"
 
