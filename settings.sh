28b9b33c5a804b5506f6f35570e2a042240df42b
Leonid Pilyugin <l.pilyugin04@gmail.com>
2024-04-07 20:48:58 +0300

    settings comments

--- settings.sh
+++ settings.sh
@@ -1,4 +1,12 @@
+# MPV starting command
 COMMAND="mpv --vo=drm --keep-open=yes"
+
+# Path where viewed file is saved
+# It is deleted on ms.sh exit
 LOCAL_FILE="/tmp/mpv-over-ssh/current"
+
+# FIFO file path
+# FIFO file is created both on local and remote machines
+# It is deleted on ms.sh exit
 FIFO_PATH="$HOME/.local/share/mpv-over-ssh/queue"
 
